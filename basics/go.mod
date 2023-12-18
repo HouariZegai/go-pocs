0a761658fe766102e899c49d7fbcda66602d6ae6
HouariZegai <houarizegai14@gmail.com>
2023-12-18 09:57:43 +0100

    Playing with Go basics

--- basics/go.mod
+++ basics/go.mod
@@ -1,3 +1,3 @@
-module houarizegai.com/basics
+module basics
 
 go 1.21.5
--- basics/main.go
+++ basics/main.go
@@ -5,20 +5,28 @@ import (
 	"strings"
 )
 
+// Package level variable
+const appName = "GoLang Basics Application"
+
+var tickets = 10
+
 func main() {
-	// For loop
+	fmt.Println("\nCall a function")
+	greeting()
+
+	fmt.Println("\nFor loop")
 	for i := 0; i < 3; i++ {
 		fmt.Println("For loop", i)
 	}
 
-	// While loop
+	fmt.Println("\nWhile loop")
 	i := 0
 	for i < 2 {
 		fmt.Println("While loop", i)
 		i++
 	}
 
-	// Data types and pointer
+	fmt.Println("\nData types and pointer")
 	var age int
 	age = 10
 	fmt.Printf("Age type is: %T\n", age)
@@ -38,7 +46,7 @@ func main() {
 	fmt.Println("Names: ", names)
 	fmt.Println("Size of the array: ", len(names))
 
-	// Slices
+	fmt.Println("\nSlices")
 	var notes []int
 	notes = append(notes, 1)
 	fmt.Println("Notes: ", notes)
@@ -46,16 +54,26 @@ func main() {
 	notes = append(notes, 33)
 	fmt.Println("Size of the slice: ", len(notes))
 
-	// Other way to declare a slice
+	fmt.Println("\nOther way to declare a slice")
 	notes2 := []int{}
 	fmt.Println("Notes2: ", notes2)
 
-	// Read user input
+	fmt.Println("\nRead user input")
 	var name string
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&name)
 	fmt.Println("Your name is: ", name)
 	fmt.Println("Memory address of name: ", &name)
+
+	fmt.Println("\nIf Condition")
+	num := 1
+	if num < 1 {
+		fmt.Println("Num is less than 1")
+	} else if num == 1 {
+		fmt.Println("Num is equal to 1")
+	} else {
+		fmt.Println("Num is greater than 1")
+	}
 
 	index := 0
 	for {
@@ -67,11 +85,72 @@ func main() {
 	}
 
 	var books = []string{"Clean code", "Effective Java"}
-	// Foreach
+
+	fmt.Println("\nFor Each")
 	for index, book := range books {
 		fmt.Printf("Book[%v]: %v\n", index, book)
 		fmt.Println("First word of the book: ", strings.Fields(book)[0]) // split the string by space
 	}
 
 	fmt.Println(append(books, "Clean Architecture"))
+
+	if len(books) > 0 {
+		fmt.Println("We have some books!")
+	} else {
+		fmt.Println("We don't have any books!")
+	}
+
+	if len(books) == 2 {
+		fmt.Println("We have two books!")
+	}
+
+	fmt.Println("\nLoop with validation")
+	for {
+		fmt.Print("Enter your name (Mohamed): ")
+		fmt.Scan(&name)
+		if name != "Mohamed" {
+			continue
+		}
+		fmt.Println("Welcome Mohamed!")
+		break
+	}
+
+	for {
+		var email string
+		fmt.Print("Enter your email: ")
+		fmt.Scan(&email)
+		validEmail, alwaysTrue := validEmail(email)
+		if !validEmail {
+			continue
+		}
+		fmt.Println("Always true: ", alwaysTrue)
+		fmt.Println("Thanks for providing a valid email!")
+		break
+	}
+
+	fmt.Println("\nWhile loop")
+	counter := 0
+	for counter < 2 && true {
+		fmt.Println("Counter: ", counter)
+		counter++
+	}
+
+	fmt.Println("\nSwitch Statement")
+	city := "Algiers"
+	switch city {
+	case "Oran", "Tiaret", "Tlemcen":
+		fmt.Println("City is located in the West of Algeria")
+	case "Algiers", "Blida":
+		fmt.Println("City is located in the Center of Algeria")
+	default:
+		fmt.Println("City is not located in West nor Center of Algeria")
+	}
+}
+
+func greeting() {
+	fmt.Println("Welcome to our", appName)
+}
+
+func validEmail(email string) (bool, bool) {
+	return len(email) > 2 && strings.Contains(email, "@"), true
 }
